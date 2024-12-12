aeda3f8b6c34a515678978151651ee7589884065
Tanishk Kumar <kumartanishk216@gmail.com>
2024-12-12 21:49:00 +0530

    Add files via upload

---
+++ dict.mathods.py
@@ -0,0 +1,9 @@
+marks = {
+    "Tanishk": 100,
+    "piyush": 78,
+    "Himanshi": 97,
+}
+
+#marks.update({"Tanishk": 99})
+print(marks.get("Tanishk2")) #prints None
+print(marks["Tanishk2"]) #Returns an error
---
+++ dict.py
@@ -0,0 +1,7 @@
+marks = {
+    "Tanishk": 100,
+    "piyush": 78,
+    "Himanshi": 97,
+}
+
+print(marks,type(marks))
